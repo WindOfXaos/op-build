fd07a1a6bd67cc62db717547967a463d46329647
WindOfXaos <mouaz30970893@f-eng.tanta.edu.eg>
2021-12-10 17:19:19 +0200

    fix bugs

--- secret/secret_token.py
+++ secret/secret_token.py
@@ -1,5 +1,5 @@
 # This should be in src/secret/secret_token.py
 class token_class():
     def get_token(self):
-        self.token = 'OTE2ODUwNTM5NjI5NzkzMzMx.YawJmg.wkxeKE7yg--qlM18pS8GRZ0NTs4'
+        self.token = 'd2QMucTAy5vA8MNqBY0ht0rHaZHbp-dd'
         return self.token

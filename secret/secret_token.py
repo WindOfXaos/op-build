03878a8d3867d2610ab406f935c2c9205177ec66
WindOfXaos <mouaz30970893@f-eng.tanta.edu.eg>
2021-12-10 17:24:16 +0200

    fix bugs 2

--- secret/secret_token.py
+++ secret/secret_token.py
@@ -1,5 +1,5 @@
 # This should be in src/secret/secret_token.py
 class token_class():
     def get_token(self):
-        self.token = 'd2QMucTAy5vA8MNqBY0ht0rHaZHbp-dd'
+        self.token = 'OTE2ODUwNTM5NjI5NzkzMzMx.YawJmg.emfheuSCaW1wbUNTNpACe2RfaJk'
         return self.token

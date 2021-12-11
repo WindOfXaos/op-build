69e43d8a653a8f3cca87d6528403b7551eadbc69
WindOfXaos <mouaz30970893@f-eng.tanta.edu.eg>
2021-12-11 04:27:22 +0200

    token management

--- secret/secret_token.py
+++
@@ -1,5 +0,0 @@
-# This should be in src/secret/secret_token.py
-class token_class():
-    def get_token(self):
-        self.token = 'OTE2ODUwNTM5NjI5NzkzMzMx.YawJmg.emfheuSCaW1wbUNTNpACe2RfaJk'
-        return self.token
